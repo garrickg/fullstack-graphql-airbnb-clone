848416be824e1252845f4a70136bc098ce23274f
Garrick Gunn <ggunn@starfishmedical.com>
2019-12-31 15:57:32 -0800

    adding design library

---
+++ packages/web/src/modules/register/ui/RegisterView.tsx
@@ -0,0 +1,8 @@
+import * as React from "react";
+import Button from "antd/lib/button";
+
+export class RegisterView extends React.PureComponent {
+  render() {
+    return <Button type="primary">Button</Button>;
+  }
+}
